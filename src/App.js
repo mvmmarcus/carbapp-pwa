858284400288d2e51080b8c6c46c2822b4ea627e
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-08-09 02:43:47 -0300

    feat: test write to racp

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import "./App.css";
 function App() {
   const [descriptorWriteValue, setDescriptorWriteValue] = useState("");
   const [descriptor, setDescriptor] = useState(null);
+  const [characteristic, setCharacteristic] = useState(null);
+  const [characteristicWriteValue, setCharacteristicWriteValue] =
+    useState(null);
 
   useEffect(() => {}, []);
+
+  function handleCharacteristicValueChanged(event) {
+    const value = event.target.value;
+    console.log("Received " + value);
+    // TODO: Parse Heart Rate Measurement value.
+    // See https://github.com/WebBluetoothCG/demos/blob/gh-pages/heart-rate-sensor/heartRateSensor.js
+  }
 
   function onReadButtonClick() {
     console.log("Requesting any Bluetooth Device...");
@@ -29,8 +39,15 @@ function App() {
           "00002a52-0000-1000-8000-00805f9b34fb"
         );
       })
+      .then((characteristic) => characteristic.startNotifications())
       .then((characteristic) => {
+        characteristic.addEventListener(
+          "characteristicvaluechanged",
+          handleCharacteristicValueChanged
+        );
+        console.log("Notifications have been started.");
         console.log("Getting Descriptor...");
+        setCharacteristic(characteristic);
         return characteristic.getDescriptor(
           "00002902-0000-1000-8000-00805f9b34fb"
         );
@@ -42,6 +59,7 @@ function App() {
       })
       .then((value) => {
         let decoder = new TextDecoder("utf-8");
+        console.log("descriptor value: ", value);
         console.log(
           "> Characteristic Client Description: " + decoder.decode(value)
         );
@@ -63,6 +81,26 @@ function App() {
         console.log(
           "> Characteristic User Description changed to: " +
             descriptorWriteValue
+        );
+      })
+      .catch((error) => {
+        console.log("Argh! " + error);
+      });
+  }
+
+  function onCharacteristicWriteClick() {
+    if (!characteristic) {
+      return;
+    }
+
+    let encoderValue = new TextEncoder("utf-8");
+    console.log("Setting Characteristic User Description...");
+    characteristic
+      .writeValue(encoderValue.encode(characteristicWriteValue))
+      .then((_) => {
+        console.log(
+          "> Characteristic User Description changed to: " +
+            characteristicWriteValue
         );
       })
       .catch((error) => {
@@ -100,6 +138,16 @@ function App() {
         />
         <button id="writeButton" onClick={onWriteButtonClick}>
           Set Characteristic User Description
+        </button>
+      </p>
+      <p>
+        <input
+          type="text"
+          placeholder="Write Value"
+          onChange={(event) => setCharacteristicWriteValue(event.target.value)}
+        />
+        <button id="writeButton" onClick={onCharacteristicWriteClick}>
+          Set Characteristic Write Value
         </button>
       </p>
     </div>
